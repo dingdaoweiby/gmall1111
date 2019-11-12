0c32ab68c8bd9fa8b06b8fe890cfc14d45a8ae99
dingdaoweiby <dingdaoweiby@gmail.com>
2019-11-11 21:47:56 -0500

    add to user manage module

---
+++ gmall1111-user-manage/src/main/java/com/sn/gmall1111/user/Gmall1111UserManageApplication.java
@@ -0,0 +1,13 @@
+package com.sn.gmall1111.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall1111UserManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall1111UserManageApplication.class, args);
+    }
+
+}
